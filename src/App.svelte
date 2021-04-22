5e2d23a9ace836394fc3ebe738c60b2a07fb418c
Filip Talaga <talaga.filip@gmail.com>
2021-04-22 20:50:19 +0200

    Format

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
-	export let name: string;
-	export let surname: string;
+    export let name: string;
+    export let surname: string;
+
+    const handleClick = () => {
+        console.log('click');
+    };
 </script>
 
 <main>
-	<h1>Hello {name} {surname}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello {name} {surname}!</h1>
+    <button on:click={handleClick}> Click me </button>
+    <p>
+        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+        apps.
+    </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
