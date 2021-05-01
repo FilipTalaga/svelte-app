f16fe3b8d62791e467813559e548e70245a156e1
Filip Talaga <talaga.filip@gmail.com>
2021-05-01 15:19:42 +0200

    Remove index.html redirect

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@ import NotFound from './views/NotFound.svelte';
 import type { Route } from './types';
 
 export const routes: Route[] = [
-    { path: 'index.html', redirect: '/' },
     { path: '/', component: Home },
     { path: 'dashboard', component: Dashboard },
     { path: '*', component: NotFound },
