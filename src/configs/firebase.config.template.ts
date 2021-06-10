a6e9ac692e775aaa74027dc7479d24fc46479d80
Filip Talaga <talaga.filip@gmail.com>
2021-06-10 21:56:13 +0200

    Add config template

---
+++ src/configs/firebase.config.template.ts
@@ -0,0 +1,10 @@
+/* Copy this template and save as `firebase.config.ts` with real firebase data */
+export default {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+};
